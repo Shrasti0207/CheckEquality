4b9e2bc1f8b1f599d705b71102b8122e1a1052b3
Shrasti0207 <shrasti.gupta@knoldus.com>
2023-03-31 21:25:38 +0530

    commiting the equality check file

---
+++ src/main/scala/equalityCheck.scala
@@ -0,0 +1,62 @@
+package com.knoldus
+import scala.util.{Failure, Success, Try}
+
+object equalityCheck extends App {
+  // create the instances of the case class
+  val studentOne = Student("Shrasti", 21, "BTech")
+  val studentTwo = Student("Reetesh", 23, "Btech")
+  val studentThree = Student("Shrasti", 21, "BTech")
+
+  // check using equals method
+  val resultOne = Try(studentOne.equals(studentTwo)) match {
+    case Success(result) => println(result)
+    case Failure(exception) => false
+  }
+  val resultTwo = Try(studentOne.equals(studentThree)) match {
+    case Success(result) => println(result)
+    case Failure(exception) => false
+  }
+
+  // check using == operator
+  val resultThree = Try(studentOne == studentTwo) match {
+    case Success(result) => println(result)
+    case Failure(exception) => false
+  }
+  val resultFour = Try(studentOne == studentThree) match {
+    case Success(result) => println(result)
+    case Failure(exception) => false
+  }
+
+  // check using != opertor
+  val resultFive = Try(studentOne != studentTwo) match {
+    case Success(result) => println(result)
+    case Failure(exception) => false
+  }
+  val resultSix = Try(studentOne != studentThree) match {
+    case Success(result) => println(result)
+    case Failure(exception) => false
+  }
+
+  // check using ne method
+  val resultSeven = Try(studentOne.ne(studentTwo)) match {
+    case Success(result) => println(result)
+    case Failure(exception) => false
+  }
+  val resultEight = Try(studentOne.ne(studentThree)) match {
+    case Success(result) => println(result)
+    case Failure(exception) => false
+  }
+
+  // using eq method
+  val result1 = Try(studentOne.eq(studentTwo)) match {
+    case Success(result) => println(result)
+    case Failure(exception) => false
+  }
+  val result2 = Try(studentOne.eq(studentThree)) match {
+    case Success(result) => println(result)
+    case Failure(exception) => false
+  }
+}
+
+case class Student(name: String, rollNo: Int, department: String)
+
---
+++ src/test/scala/EqualityCheckSpecTest.scala
@@ -0,0 +1,31 @@
+package com.knoldus
+import org.scalatest.funsuite.AnyFunSuite
+class EqualityCheckSpecTest extends AnyFunSuite{
+  val studentOne = Student("Shrasti", 21, "BTech")
+  val studentTwo = Student("Reetesh", 23, "Btech")
+  val studentThree = Student("Shrasti", 21, "BTech")
+  test("Test for equals method"){
+    assert(studentOne.equals(studentThree))
+  }
+
+  test("Test for equals method - unequal") {
+    assert(!studentOne.equals(studentTwo))
+  }
+
+  test("Test for == operator ") {
+    assert(studentOne == studentThree)
+  }
+
+  test("Test for != operator ") {
+    assert(studentOne != studentTwo)
+  }
+
+  test("Test for ne method ") {
+    assert(studentOne.ne(studentThree))
+  }
+
+  test("Test for eq method ") {
+    assert(!studentOne.eq(studentTwo))
+  }
+
+}
